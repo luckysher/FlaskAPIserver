47dd77d351624539481d4c226cd39c2477e626d9
luckysher <luckyshera95@gmail.com>
2018-04-11 20:49:20 +0530

    added songs utils files with json

---
+++ utils/songsUtils.py
@@ -0,0 +1,10 @@
+import json
+import traceback
+
+
+class SongsUtils:
+    """
+    Class for Managing songs in json file. This class
+    have the various methods for managing the songs like
+    fetching songs list, fetching songs by artist etc..
+    """
